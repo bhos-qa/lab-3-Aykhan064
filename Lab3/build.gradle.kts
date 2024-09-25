516adc9e1826e787cf4b90b0c633defea1557e1f
Aykhan Kazimov <ayxan.kazimov.03@mail.ru>
2024-09-25 12:27:29 +0400

    Forth commit

--- Lab3/build.gradle.kts
+++ Lab3/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     id("jacoco")
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
 group = "org.example"
@@ -13,16 +14,39 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    implementation("com.google.guava:guava:31.0.1-jre")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "failed", "skipped")
+    }
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-    dependsOn(tasks.test) 
+    dependsOn(tasks.test)
     reports {
-        xml.required.set(true) 
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+jacoco {
+    toolVersion = "0.8.11" // Use the latest stable version of Jacoco
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "lab-3-Aykhan064")
+        property("sonar.projectName", "lab-3-Aykhan064")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.language", "kotlin")
+    }
 }
